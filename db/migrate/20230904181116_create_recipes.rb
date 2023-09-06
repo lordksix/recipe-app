ce278960218b58a332fbb3e8c002542d41e8299a
lordksix <121689749+lordksix@users.noreply.github.com>
2023-09-05 22:23:58 -0500

    Setup recipe_foods models

--- db/migrate/20230904181116_create_recipes.rb
+++ db/migrate/20230904181116_create_recipes.rb
@@ -1,7 +1,7 @@
 class CreateRecipes < ActiveRecord::Migration[7.0]
   def change
     create_table :recipes do |t|
-      t.string :name
+      t.string :name, null: false
       t.datetime :preparation_time
       t.datetime :cooking_time
       t.text :description
--- db/migrate/20230904183412_create_recipe_foods.rb
+++ db/migrate/20230904183412_create_recipe_foods.rb
@@ -2,10 +2,8 @@ class CreateRecipeFoods < ActiveRecord::Migration[7.0]
   def change
     create_table :recipe_foods do |t|
       t.text :quantity
-      t.integer :recipe_id
-      t.integer :food_id
-      t.references :user, null: false, foreign_key: true
       t.references :food, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true
 
       t.timestamps
     end
