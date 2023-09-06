863274100f879fe20b831d76eaedb73b10cb8476
lordksix <121689749+lordksix@users.noreply.github.com>
2023-09-05 22:17:10 -0500

    Implement recipe model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :foods, foreign_key: 'user_id'
+  has_many :recipes, foreign_key: 'user_id'
 end
