9ca702f273d87f7243560fe0bf03bc575776adf8
lordksix <121689749+lordksix@users.noreply.github.com>
2023-09-05 22:43:04 -0500

    Setup food authorization rules

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,3 +1,10 @@
 class Ability
   include CanCan::Ability
+  def initialize(user)
+    user ||= User.new
+
+    can %i[index show], User
+    can [:new], Food
+    can %i[index update destroy], Food, { user_id: user.id }
+  end
 end
