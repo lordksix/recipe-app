5b4d6faeeffe93c3e974cbadc5dddba186b8628b
tumainimaganiko <tumainimaganiko@gmail.com>
2023-09-08 15:33:42 +0300

    Add Fix Linters

--- spec/requests/recipe_foods_spec.rb
+++ spec/requests/recipe_foods_spec.rb
@@ -1,32 +1,31 @@
 require 'rails_helper'
 
-RSpec.describe "RecipeFoods", type: :request do
-  describe "GET /index" do
-    it "returns http success" do
-      get "/recipe_foods/index"
+RSpec.describe 'RecipeFoods', type: :request do
+  describe 'GET /index' do
+    it 'returns http success' do
+      get '/recipe_foods/index'
       expect(response).to have_http_status(:success)
     end
   end
 
-  describe "GET /destroy" do
-    it "returns http success" do
-      get "/recipe_foods/destroy"
+  describe 'GET /destroy' do
+    it 'returns http success' do
+      get '/recipe_foods/destroy'
       expect(response).to have_http_status(:success)
     end
   end
 
-  describe "GET /update" do
-    it "returns http success" do
-      get "/recipe_foods/update"
+  describe 'GET /update' do
+    it 'returns http success' do
+      get '/recipe_foods/update'
       expect(response).to have_http_status(:success)
     end
   end
 
-  describe "GET /edit" do
-    it "returns http success" do
-      get "/recipe_foods/edit"
+  describe 'GET /edit' do
+    it 'returns http success' do
+      get '/recipe_foods/edit'
       expect(response).to have_http_status(:success)
     end
   end
-
 end
