abfe1a9c9fc36427697d8d39982edcd87ac80125
tumainimaganiko <tumainimaganiko@gmail.com>
2023-09-08 22:32:26 +0300

    Optimize App

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -2,7 +2,7 @@ class FoodsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @foods = current_user.foods
+    @foods = current_user.foods.includes(:user)
     @current_user = current_user
   end
 
