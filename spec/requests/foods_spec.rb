43d04e5793fe5621e40f2c022edd034d9541ab29
lordksix <121689749+lordksix@users.noreply.github.com>
2023-09-08 12:57:52 -0500

    Implement model test for user reciepe and food

--- spec/requests/foods_spec.rb
+++ spec/requests/foods_spec.rb
@@ -1,14 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe 'Foods', type: :request do
-  describe 'GET /index' do
+  pending describe 'GET /index' do
     it 'returns http success' do
       get '/foods/index'
       expect(response).to have_http_status(:success)
     end
   end
 
-  describe 'GET /show' do
+  pending describe 'GET /show' do
     it 'returns http success' do
       get '/foods/show'
       expect(response).to have_http_status(:success)
--- spec/requests/recipes_spec.rb
+++ spec/requests/recipes_spec.rb
@@ -1,21 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe 'Recipes', type: :request do
-  describe 'GET /index' do
+  pending describe 'GET /index' do
     it 'returns http success' do
       get '/recipes/index'
       expect(response).to have_http_status(:success)
     end
   end
 
-  describe 'GET /show' do
+  pending describe 'GET /show' do
     it 'returns http success' do
       get '/recipes/show'
       expect(response).to have_http_status(:success)
     end
   end
 
-  describe 'GET /destroy' do
+  pending describe 'GET /destroy' do
     it 'returns http success' do
       get '/recipes/destroy'
       expect(response).to have_http_status(:success)
