78ab0710bb641233c54a92ca92efcb1172c286a4
lordksix <121689749+lordksix@users.noreply.github.com>
2023-09-09 00:06:24 -0500

    Add name to users schema

--- db/schema.rb
+++ db/schema.rb
@@ -48,9 +48,10 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_05_053228) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
+    t.string "name", default: "", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
